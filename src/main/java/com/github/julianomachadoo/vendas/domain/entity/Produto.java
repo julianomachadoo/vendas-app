066a2d100af15d289af878f433849668f6472daa
julianomachadoo <julmachadoramos@gmail.com>
2022-12-29 13:56:50 -0300

    entidade produto

---
+++ src/main/java/com/github/julianomachadoo/vendas/domain/entity/Produto.java
@@ -0,0 +1,43 @@
+package com.github.julianomachadoo.vendas.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    @Column(name = "preco_unitario")
+    private BigDecimal preco;
+
+    public Produto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
