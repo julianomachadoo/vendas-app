92d57e6c6f36fcbed22a0c11c8d8786fb1f3288f
julianomachadoo <julmachadoramos@gmail.com>
2022-12-29 15:54:02 -0300

    preparando migration

---
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -0,0 +1,33 @@
+create table categoria(
+    id bigint primary key not null auto_increment,
+    nome varchar(50) not null unique
+);
+
+create table produto(
+    id bigint primary key not null auto_increment,
+    data_cadastro datetime not null,
+    descricao varchar(255),
+    preco_unitario decimal(10,2) not null,
+    categoria_id bigint not null foreign key references categoria(id)
+);
+
+create table cliente(
+    id bigint primary key not null auto_increment,
+    nome varchar(100) not null,
+    cpf varchar(14) not null unique
+);
+
+create table pedido(
+    id bigint primary key not null auto_increment,
+    cliente_id bigint not null foreign key references cliente(id),
+    data_pedido datetime not null,
+    valor_total decimal(10,2) not null,
+    status_pedido varchar(20) not null
+);
+
+create table item_pedido(
+    id bigint primary key not null auto_increment,
+    quantidade integer not null,
+    pedido_id bigint not null foreign key references pedido(id),
+    produto_id bigint not null foreign key references produto(id)
+);
