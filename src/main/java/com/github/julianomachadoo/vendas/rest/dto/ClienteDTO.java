3c73da478b3a7b63f78a1890eeef623619f5e0f5
julianomachadoo <julmachadoramos@gmail.com>
2022-12-29 21:06:16 -0300

    ClienteDTO id + JsonIgnore

--- src/main/java/com/github/julianomachadoo/vendas/rest/dto/ClienteDTO.java
+++ src/main/java/com/github/julianomachadoo/vendas/rest/dto/ClienteDTO.java
@@ -1,14 +1,18 @@
 package com.github.julianomachadoo.vendas.rest.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.github.julianomachadoo.vendas.domain.entity.Cliente;
 
 public class ClienteDTO {
+    @JsonIgnore
+    private Long id;
     private final String nome;
     private final String cpf;
 
     public ClienteDTO(Cliente cliente) {
         this.nome = cliente.getNome();
         this.cpf = cliente.getCpf();
+        this.id = cliente.getId();
     }
 
     public String getNome() {
@@ -17,6 +21,14 @@ public class ClienteDTO {
 
     public String getCpf() {
         return cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
 
