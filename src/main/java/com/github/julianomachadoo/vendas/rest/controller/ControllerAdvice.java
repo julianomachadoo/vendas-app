4e4c486bfb9c68ba620774fa6497b7ff41eae1b0
julianomachadoo <julmachadoramos@gmail.com>
2022-12-29 21:25:17 -0300

    ControllerAdvice

---
+++ src/main/java/com/github/julianomachadoo/vendas/rest/controller/ControllerAdvice.java
@@ -0,0 +1,42 @@
+package com.github.julianomachadoo.vendas.rest.controller;
+
+import com.github.julianomachadoo.vendas.exceptions.DadosNaoEncontradosException;
+import com.github.julianomachadoo.vendas.rest.dto.ApiErrorsDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @Autowired
+    MessageSource messageSource;
+
+    @ExceptionHandler(DadosNaoEncontradosException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErrorsDTO handleDadosNaoEncontradosException(DadosNaoEncontradosException ex) {
+        return new ApiErrorsDTO(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<ApiErrorsDTO> handleValidacoes (MethodArgumentNotValidException ex) {
+        List<ApiErrorsDTO> dtoList = new ArrayList<>();
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(e -> {
+            String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+            ApiErrorsDTO erro = new ApiErrorsDTO(e.getField(), mensagem);
+            dtoList.add(erro);
+        });
+        return dtoList;
+    }
+}
