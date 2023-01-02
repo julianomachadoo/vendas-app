c2275abe0cf2f650132d2d794c0a383220de39f0
julianomachadoo <julmachadoramos@gmail.com>
2023-01-02 09:02:27 -0300

    ProdutoDTO

---
+++ src/main/java/com/github/julianomachadoo/vendas/rest/dto/ProdutoDTO.java
@@ -0,0 +1,73 @@
+package com.github.julianomachadoo.vendas.rest.dto;
+
+import com.github.julianomachadoo.vendas.domain.entity.Produto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class ProdutoDTO {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private LocalDateTime dataCadastro;
+    private String categoria;
+
+    public ProdutoDTO(Produto produto) {
+        this.id = produto.getId();
+        this.nome = produto.getNome();
+        this.descricao = produto.getDescricao();
+        this.preco = produto.getPreco();
+        this.dataCadastro = produto.getDataCadastro();
+        this.categoria = produto.getCategoria().toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
