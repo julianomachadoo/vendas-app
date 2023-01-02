d6d07b4749d68528cfde1695834837fa6abaacce
julianomachadoo <julmachadoramos@gmail.com>
2023-01-02 10:47:32 -0300

    Validacao de cadastro de produto

--- src/main/java/com/github/julianomachadoo/vendas/rest/form/ProdutoForm.java
+++ src/main/java/com/github/julianomachadoo/vendas/rest/form/ProdutoForm.java
@@ -1,12 +1,18 @@
 package com.github.julianomachadoo.vendas.rest.form;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 public class ProdutoForm {
 
+    @Length(max = 100) @NotBlank
     private String nome;
+    @Length(max = 255)
     private String descricao;
+    @NotBlank
     private String preco;
+    @NotBlank
     private String categoria;
     @NotNull
     private Integer estoque;
